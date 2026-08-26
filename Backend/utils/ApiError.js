31d8753d3b6963fd5ce8d8860476ed30e282ba95
Lakshit Paliwal <paliwallucky001@gmail.com>
2026-08-26 15:56:47 +0530

    Update ApiError.js

--- Backend/utils/ApiError.js
+++ Backend/utils/ApiError.js
@@ -9,7 +9,7 @@ class ApiError extends Error{
         this.statusCode = statusCode
         this.data = null
         this.message = message
-        this.success = false
+        this.success = true
         this.errors = errors
         
         if (stack) {
